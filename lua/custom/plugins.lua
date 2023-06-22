8f439bec261a9e3ecae068a4d53bf96c1a89a0b4
AuJung <nuttapong6280@gmail.com>
2023-06-22 17:25:03 +0700

    add custom plugin file and add copilot plugin.

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,12 @@
+local plugins = {
+    {
+        "github/copilot.vim",
+        lazy = false,
+        -- config = function()
+        --   vim.g.copilot_key = "ctrl+space"
+        --   vim.g.copilot_no_tab_map = true
+        -- end
+    },
+}
+
+return plugins
